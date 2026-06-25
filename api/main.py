17f38437f3b9e2557c64083e310d81b4c649b947
sharmarahulrs726 <sharmarahul.rs726@outlook.com>
2026-06-25 16:35:51 +0530

    cofig more api urls

--- api/main.py
+++ api/main.py
@@ -45,7 +45,11 @@ app = FastAPI(title="Tax Investigation API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://hffrotend-n2ey08i61-peronsal-rsjald.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
